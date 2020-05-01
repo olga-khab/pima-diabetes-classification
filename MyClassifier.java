2abdb4d5b93c2aa070ac9e17069b709e8b1030fa
olga-khab <olga.a.khab@gmail.com>
2020-05-01 15:27:33 +1000

    naive bayes testing

--- MyClassifier.java
+++ MyClassifier.java
@@ -4,18 +4,24 @@ import java.lang.Math.*;
 
 public class MyClassifier {
   // read in a file as a 2d arraylist
-  public static ArrayList<ArrayList<double>> readCSV(String filename){
+  public static ArrayList<ArrayList<Double>> readCSV(String filename){
     BufferedReader br = null;
     String line = "";
-    ArrayList<ArrayList<double>> file = new ArrayList<ArrayList<double>>();
+    ArrayList<ArrayList<Double>> file = new ArrayList<ArrayList<Double>>();
     try {
       br = new BufferedReader(new FileReader(filename));
       while ((line = br.readLine()) != null) {
-        ArrayList<double> double_line = new ArrayList<double>()
+        ArrayList<Double> double_line = new ArrayList<Double>();
         String[] str_line = line.split(",");
         // write line to ArrayList
         for (String s:str_line){
-          double_line.add(Double.parseDouble(s));
+          if (s.compareTo("yes")==0){
+            double_line.add(1.0);
+          }else if (s.compareTo("no")==0){
+            double_line.add(0.0);
+          }else{
+            double_line.add(Double.parseDouble(s));
+          }
         }
         file.add(double_line);
       }
@@ -26,11 +32,11 @@ public class MyClassifier {
   }
 
   public static void main(String[] args){
-    ArrayList<ArrayList<double>> training = readCSV(args[0])
-    ArrayList<ArrayList<double>> testing = readCSV(args[1])
+    ArrayList<ArrayList<Double>> training = readCSV(args[0]);
+    ArrayList<ArrayList<Double>> testing = readCSV(args[1]);
 
-    if (args[2].compareTo("NB")){
-      for (ArrayList<double> line : testing){
+    if (args[2].compareTo("NB")==0){
+      for (ArrayList<Double> line : testing){
         // prints determined class
         NB.NB(training, line);
       }
--- NB.java
+++ NB.java
@@ -1,18 +1,21 @@
 import java.lang.*;
+import java.util.*;
 public class NB{
-  public static void NB(ArrayList<ArrayList<double>> training,
-  <ArrayList<double> testing){
-    ArrayList<ArrayList<double>> class_yes = new ArrayList<ArrayList<double>>();
-    ArrayList<ArrayList<double>> class_no = new ArrayList<ArrayList<double>>();
-    for (ArrayList<double> row : training){
-      if row.get(8).compareTo("yes"){
+  public static void NB(ArrayList<ArrayList<Double>> training,
+  ArrayList<Double> testing){
+    ArrayList<ArrayList<Double>> class_yes = new ArrayList<ArrayList<Double>>();
+    ArrayList<ArrayList<Double>> class_no = new ArrayList<ArrayList<Double>>();
+    for (ArrayList<Double> row : training){
+      if (row.get(8)==1.0){
         class_yes.add(row);
       }else{
         class_no.add(row);
       }
     }
-    double p_yes = p_class_E(class_pdf(class_yes));
-    double p_no = p_class_E(class_pdf(class_no));
+    double p_yes = p_class_E(class_pdf(class_yes, testing));
+    double p_no = p_class_E(class_pdf(class_no, testing));
+    //System.out.println(p_yes);
+    //System.out.println(p_no);
     if (p_yes>=p_no){
       System.out.println("yes");
     }else{
@@ -21,7 +24,7 @@ public class NB{
   }
 
 
-  public static double mean(ArrayList<double> vals){
+  public static double mean(ArrayList<Double> vals){
     double sum = 0;
     for (double e : vals){
       sum+=e;
@@ -29,7 +32,7 @@ public class NB{
     return sum/vals.size();
   }
 
-  public static double stdev(ArrayList<double> vals){
+  public static double stdev(ArrayList<Double> vals){
     double mean = mean(vals);
     double sum_sq = 0;
     for (double e : vals){
@@ -38,13 +41,16 @@ public class NB{
     return Math.sqrt(sum_sq/(vals.size()-1));
   }
 
-  public static double calculatePdf(double x, ArrayList<double> vals){
+  public static double calculatePdf(double x, ArrayList<Double> vals){
     double stdev = stdev(vals);
     double mean = mean(vals);
-    return (Math.exp(-(x-mean)/(2*stdev*stdev)))/(stdev*Math.sqrt(2*Math.pi));
+    //System.out.println("pdf: ");
+    //System.out.println((x-mean));
+    //System.out.println((Math.exp(-(x-mean)*(x-mean)/(2*stdev*stdev)))/(stdev*Math.sqrt(2*Math.PI)));
+    return ((Math.exp(-(x-mean)*(x-mean)/(2*stdev*stdev)))/(stdev*Math.sqrt(2*Math.PI)));
   }
 
-  public static double p_class_E(ArrayList<double> pdfs){
+  public static double p_class_E(ArrayList<Double> pdfs){
     double product=1;
     for (double e:pdfs){
       product*=e;
@@ -52,13 +58,14 @@ public class NB{
     return product;
   }
 
-  public static ArrayList<double> class_pdf(ArrayList<ArrayList<double>> class){
-    ArrayList<double> pdf = new ArrayList<double>();
+  public static ArrayList<Double> class_pdf(ArrayList<ArrayList<Double>> class_rows,
+  ArrayList<Double> testing){
+    ArrayList<Double> pdf = new ArrayList<Double>();
     // for each class calculate P(class|E)
     for (int i=0; i< 8; i++){ // iterate through each column
-      ArrayList<double> col = new A<ArrayList<double>();
-      for (int j=0;j<class.size();j++){
-        col.add(class.get(j).get(i));
+      ArrayList<Double> col = new ArrayList<Double>();
+      for (int j=0;j<class_rows.size();j++){
+        col.add(class_rows.get(j).get(i));
       }
       // for the column and testing value, clculate pdf
       pdf.add(calculatePdf(testing.get(i),col));
